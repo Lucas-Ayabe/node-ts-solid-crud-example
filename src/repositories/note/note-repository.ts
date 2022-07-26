72d29c104e3de656e6a11fe53b48592afcb202a4
Lucas-Ayabe <lucas.ayabe@gmail.com>
2022-07-26 18:04:20 -0300

    refactor: applying interface segregation principle

--- src/repositories/note/note-repository.ts
+++ src/repositories/note/note-repository.ts
@@ -2,10 +2,24 @@ import { Note } from "../../models";
 
 export type CreateNoteDto = Pick<Note, "title">;
 
-export interface NoteRepository {
+export interface ReadNoteRepository {
   findAll(): Promise<Note[]>;
   findById(id: number): Promise<Note>;
+}
+
+export interface CreateNoteRepository {
   create(note: CreateNoteDto): Promise<Note>;
+}
+
+export interface UpdateNoteRepository {
   update(note: Note): Promise<void>;
+}
+
+export interface DeleteNoteRepository {
   delete(id: number): Promise<void>;
 }
+
+export type NoteRepository = ReadNoteRepository &
+  CreateNoteRepository &
+  UpdateNoteRepository &
+  DeleteNoteRepository;
